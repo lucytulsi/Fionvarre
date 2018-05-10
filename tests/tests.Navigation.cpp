e7c9121104bdd16638af437ec83f4e94cf71d536
Lucy <tykki.dyw@gmail.com>
2018-05-10 21:13:20 +0100

    failing test for successful navigating

--- tests/tests.Navigation.cpp
+++ tests/tests.Navigation.cpp
@@ -22,9 +22,71 @@ void NavigationTests::Can_not_move_when_no_location_is_set()
    assert(player.MoveWest() == false);
 }
 
+void NavigationTests::Can_move_north_when_location_is_set()
+{
+   string startDescription = "\nStart\n";
+   string northDescription = "\nYou moved north\n";
+
+   Location start(startDescription);
+   Location north(northDescription);
+
+   Player player(start);
+
+   assert(player.MoveNorth() == true);
+   assert(player.Look() == northDescription);
+}
+
+void NavigationTests::Can_move_east_when_location_is_set()
+{
+   string startDescription = "\nStart\n";
+   string eastDescription = "\nYou moved east\n";
+
+   Location start(startDescription);
+   Location east(eastDescription);
+
+   Player player(start);
+
+   assert(player.MoveEast() == true);
+   assert(player.Look() == eastDescription);
+}
+
+void NavigationTests::Can_move_south_when_location_is_set()
+{
+   string startDescription = "\nStart\n";
+   string southDescription = "\nYou moved south\n";
+
+   Location start(startDescription);
+   Location south(southDescription);
+
+   Player player(start);
+
+   assert(player.MoveSouth() == true);
+   assert(player.Look() == southDescription);
+}
+
+void NavigationTests::Can_move_west_when_location_is_set()
+{
+   string startDescription = "\nStart\n";
+   string westDescription = "\nYou moved west\n";
+
+   Location start(startDescription);
+   Location west(westDescription);
+
+   Player player(start);
+
+   assert(player.MoveWest() == true);
+   assert(player.Look() == westDescription);
+}
+
 void NavigationTests::RunAll()
 {
    printf("Running Navigation Tests...\n");
+
    Can_not_move_when_no_location_is_set();
+   Can_move_north_when_location_is_set();
+   Can_move_east_when_location_is_set();
+   Can_move_south_when_location_is_set();
+   Can_move_west_when_location_is_set();
+
    printf("All Navigation Tests Pass!!!\n\n");
 }
